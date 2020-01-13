dfa8f6bcd965791893b54e2923aab932ab766ca2
Te Piha Niha <ttrwwn@hotmail.com>
2020-01-13 19:29:28 +1300

    day-1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # KaisCodingChallenge
 "A piece of code a day, keeps the doctor away!"
+
+This is a basic 30 day challenge where you write at least a single line of code. 
+
+The rest it up to you young one.
+
---
+++ day-01.js
@@ -0,0 +1,17 @@
+// create some calculationg functions
+
+function add (a, b){
+    return a + b
+}
+
+console.log('this is going to add ' + add(12, 12))
+
+function subby (a, b){
+    return a - b
+}
+console.log('This is how many people im going to make smile ' + subby(10000000, 1000000))
+
+function multiply (a, b){
+    return a*b
+}
+console.log('this is how many people are going to see this ' + multiply(10,10))
