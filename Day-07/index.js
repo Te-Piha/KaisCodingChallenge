2ee5fb3cb27df3e473055be1119e8a1ea1e2e4f4
Te Piha Niha <ttrwwn@hotmail.com>
2020-01-19 19:37:28 +1300

    Finish day 7

---
+++ Day-07/index.js
@@ -0,0 +1,7 @@
+function addTask(){
+    const task = document.getElementById('toDo').value
+    const  listArr = []
+
+    const newListArr = listArr.push(document.getElementById('list').innerHTML += task + '<br>')
+
+}
